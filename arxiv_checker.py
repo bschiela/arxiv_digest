22cb0aef538a5a00e737c3d822a1ed022f3db4bf
wfsch <bschiela@gmail.com>
2021-03-24 15:44:06 -0400

    Build email body from query results

--- arxiv_checker.py
+++ arxiv_checker.py
@@ -4,6 +4,7 @@ from configparser import ConfigParser
 import arxiv
 
 from query_builder import build_author_query
+from email_sender import send_email
 
 logging.basicConfig(filename='info.log', level=logging.INFO, format="%(asctime)s [%(levelname)s] %(name)s: %(message)s")
 logger = logging.getLogger(__name__)
@@ -14,14 +15,17 @@ config.read('config.ini')
 
 results = {}
 for section in config.sections():
-    results[section] = []
+    results[section] = {}
     for author in config[section].keys():
-        results[section].append(
-                arxiv.query(
+        results[section][author] = arxiv.query(
                     query=build_author_query(author),
                     max_results=1,
                     sort_by="lastUpdatedDate",
                     sort_order="descending"
-                )
         )
+send_email(results)
+
+# TODO update last_checked time and limit future queries, increase max_results, run
+# every week
+
 logger.info('...done.')
--- email_sender.py
+++ email_sender.py
@@ -3,6 +3,9 @@ import smtplib
 import ssl
 from configparser import ConfigParser
 from email.message import EmailMessage
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from time import strftime
 
 logger = logging.getLogger(__name__)
 
@@ -10,18 +13,45 @@ config = ConfigParser()
 config.read('creds.ini')
 defaults = config.defaults()
 
-msg = EmailMessage()
-msg.set_content('test content')
-msg['Subject'] = 'test subject'
-msg['From'] = defaults['from_email']
-msg['To'] = defaults['to_email']
+def send_email(results):
+    msg = MIMEMultipart("alternative")
+    msg['Subject'] = 'arXiv digest'
+    msg['From'] = defaults['from_email']
+    msg['To'] = defaults['to_email']
 
-try:
-    server = smtplib.SMTP('smtp.gmail.com', 587)
-    context = ssl.create_default_context()
-    server.starttls(context=context)
-    server.login(defaults['from_email'], defaults['pw'])
-    server.send_message(msg)
-    server.quit()
-except Exception as e:
-    logger.exception(e)
+    body = build_body(results)
+    # client should attempt to render the last part first
+    msg.attach(MIMEText(body, 'plain'))
+    msg.attach(MIMEText(body, 'html'))
+
+    try:
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        context = ssl.create_default_context()
+        server.starttls(context=context)
+        server.login(defaults['from_email'], defaults['pw'])
+        server.send_message(msg)
+        server.quit()
+    except Exception as e:
+        logger.exception(e)
+
+def build_body(results):
+    """Construct raw HTML of the email body."""
+    body = "<html><body>"
+    for section in results:
+        body += "<section>"
+        body += f"<hr><h1>{section}</h1><hr>"
+        for author in results[section]:
+            for paper in results[section][author]:
+                body += f"<h2>{paper['title']}</h2>"
+                body += f"<p>{', '.join(paper['authors'])}</p>"
+                submitted = paper['published_parsed']
+                updated = paper['updated_parsed']
+                if submitted == updated:
+                    body += f"<p>submitted: {strftime('%A %b %d, %Y', submitted)}</p>"
+                else:
+                    body += f"<p>updated: {strftime('%A %b %d, %Y', updated)} (submitted: {strftime('%b %d, %Y', submitted)})</p>"
+                body += f"<a href={paper['arxiv_url']}>{'/'.join(paper['arxiv_url'].split('/')[-2:])}</a> (<a href={paper['pdf_url']}>pdf</a>)"
+                body += f"<p><b>abstract:</b> {paper['summary']}</p>"
+        body += "</section>"
+    body += "</body></html>"
+    return body
