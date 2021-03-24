c800f13f907c93a0f36acc1b105e743f79051b79
wfsch <bschiela@gmail.com>
2021-03-24 00:53:44 -0400

    Refactor author query builder to separate module

--- arxiv_checker.py
+++ arxiv_checker.py
@@ -1,26 +1,15 @@
-import configparser
 import logging
+from configparser import ConfigParser
 
 import arxiv
+
+from query_builder import build_author_query
 
 logging.basicConfig(filename='info.log', level=logging.INFO, format="%(asctime)s [%(levelname)s] %(message)s")
 logging.info('Checking arXiv...')
 
-config = configparser.ConfigParser(allow_no_value=True)
+config = ConfigParser(allow_no_value=True)
 config.read('config.ini')
-
-def build_author_query(author):
-    """Build author query string with all permutations of full names and initials."""
-    name = author.split(' ')
-    first, last = name[0], name[-1]
-    query=f"au:{last}_{first} OR au:{last}_{first[0]}"
-    if len(name) > 2:
-        middle = name[1:-1]
-        if len(middle) > 1:
-            logging.warning(f'Multiple middle names found: {first} {middle} {last}')
-        middle = middle[0]
-        query += f" OR au:{last}_{first}_{middle} OR au:{last}_{first}_{middle[0]} OR au:{last}_{first[0]}_{middle[0]}"
-    return query
 
 results = {}
 for section in config.sections():
---
+++ query_builder.py
@@ -0,0 +1,12 @@
+def build_author_query(author):
+    """Build author query string with all permutations of full names and initials."""
+    name = author.split(' ')
+    first, last = name[0], name[-1]
+    query=f"au:{last}_{first} OR au:{last}_{first[0]}"
+    if len(name) > 2:
+        middle = name[1:-1]
+        if len(middle) > 1:
+            logging.warning(f'Multiple middle names found: {first} {middle} {last}')
+        middle = middle[0]
+        query += f" OR au:{last}_{first}_{middle} OR au:{last}_{first}_{middle[0]} OR au:{last}_{first[0]}_{middle[0]}"
+    return query
